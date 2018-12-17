317349f42d54b5a87aad8331c71e6bcac4e16d83
Zanetti <znti.rafael@gmail.com>
2018-12-17 15:56:24 -0200

    Add basic MQTT communication structure

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from sense_emu import SenseHat
+import paho.mqtt.client as mqtt
+import json
 import time
 
+#
+# Properties definition
+tenant_name = "admin"
+device_id = "a6559f"
 
 #
-# Raspberry Pi's Sensor initialization
-sense = SenseHat()
+# Internal definitions setup
+client_id = "{}:{}".format(tenant_name, device_id)
+topic_to_publish = "/{}/{}/attrs".format(tenant_name, device_id)
 
+#
+# MQTT Client setup and connection
+client = mqtt.Client(client_id)
+
+print("Connecting to mqtt broker")
+client.connect(host='localhost', port=1883)
+client.loop_start()
 
 #
 # Execution loop
 while True:
-    print("Temperatura:", sense.temperature,
-            "; Pressão:", sense.pressure,
-            "; Umidade:", sense.humidity)
+    message = {
+        "temperature" : 35.11,
+        "pressure": 773.29,
+        "humidity": 66.23
+    }
+    print("Publishing", message, 'to topic', topic_to_publish)
+    client.publish(topic_to_publish, json.dumps(message))
     time.sleep(4)
